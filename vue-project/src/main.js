50c2f3a5504a3ff9a42d4c9e3a5a49d69b638955
mmoran314 <mmoran9@ewu.edu>
2023-11-04 14:36:00 -0700

    Added Vuetify and Theme for Front End
    Idk if you all will need to do this, but I installed vuetify. The commands I used were:
    npm install vuetify --save
    npm install @mdi/font -D
    npm i vue-material-design-icons
    
    We also now have our first theme with 4 RGB values available to change

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -3,4 +3,41 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { mdi } from 'vuetify/iconsets/mdi'
+
+import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify' 
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 createApp(App).mount('#app')
+
+
+// Provide the useDisplay to other components so that it can be used in testing.
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi
+    }
+  },
+  theme: {
+    defaultTheme: 'dark',
+    themes: {
+      dark: {
+        colors: {
+          primary: '#4CAF50',
+          secondary: '#FFC107',
+          tertiary: '#F44336',
+          altColor: '#222222'
+        }
+      }
+    }
+  }
+})
