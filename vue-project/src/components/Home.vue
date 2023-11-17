a07d135fe7ab873405f1c51f91dddfa60db177dd
Evan Bartels <ebartels1@ewu.edu>
2023-11-16 18:55:45 -0800

    What I had for the home.vue, can't seem to get it to work in our app though

---
+++ vue-project/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome to my website!</h1>
+    <p>Thanks for visiting. Please take a look around.</p>
+    <slot name="text"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+h1 {
+  font-size: 3rem;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+p {
+  font-size: 1.5rem;
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
