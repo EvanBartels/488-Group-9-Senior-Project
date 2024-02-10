2ae0e3b48e712cae83d65620d37ffe94c8d13c45
mmoran314 <mmoran9@ewu.edu>
2024-02-09 18:49:02 -0800

    Introduce API again

---
+++ api/CSCD490SeniorProjectApi/Controllers/DrinkRecipeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.AccessControl;
+
+namespace CSCD490SeniorProjectApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrinkRecipeController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public DrinkRecipeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [Route("GetIngredients")]
+        public JsonResult GetIngredients()
+        {
+            string query = "select distinct ingredientName from dbo.drinkRecipes";
+            DataTable dataTable = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    dataTable.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(dataTable);
+        }
+    }
+}
--- api/CSCD490SeniorProjectApi/obj/Debug/net8.0/CSCD490SeniorProjectApi.AssemblyInfo.cs
+++ api/CSCD490SeniorProjectApi/obj/Debug/net8.0/CSCD490SeniorProjectApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSCD490SeniorProjectApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b8ee9b9b1392de977832ad590dc908b777c085")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1efd88e544a65681054d9f5092375d1e0cbd0d25")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSCD490SeniorProjectApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSCD490SeniorProjectApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
