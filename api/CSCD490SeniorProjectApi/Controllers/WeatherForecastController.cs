d0e85f4c20f52da0f6c7631c3db2601fc2fe37ca
mmoran314 <mmoran9@ewu.edu>
2024-02-12 23:44:43 -0800

    Test Packed in API

--- api/CSCD490SeniorProjectApi/Controllers/WeatherForecastController.cs
+++ api/CSCD490SeniorProjectApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace CSCD490SeniorProjectApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
