bf09084f2aba476401bf488a5b872fb52b9aaa02
mmoran314 <mmoran9@ewu.edu>
2024-02-28 18:09:51 -0800

    All favorite drink queries

--- api/CSCD490SeniorProjectApi/obj/Debug/net8.0/CSCD490SeniorProjectApi.AssemblyInfo.cs
+++ api/CSCD490SeniorProjectApi/obj/Debug/net8.0/CSCD490SeniorProjectApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSCD490SeniorProjectApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83a5ee9564986f72f5c323b53009846191446fae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80e2a09d7dd39fd11324e8e7c8b283a01fde471")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSCD490SeniorProjectApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSCD490SeniorProjectApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
