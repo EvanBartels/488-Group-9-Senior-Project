1725abdfa9e52b9be44774cd1fbb7d29d2ee38f2
Evan Bartels <ebartels1@ewu.edu>
2024-03-08 15:35:48 -0800

    Added change password feature and cleaned up settings page
    Added functionality for a logged in user to change their password in settings. Adjusted the settings page to look a little more presentable on the front end.

--- vue-project/src/components/Settings.vue
+++ vue-project/src/components/Settings.vue
@@ -8,7 +8,7 @@
               <div v-if="error" class="alert alert-danger">{{error}}</div>
               <form action="#"  @submit.prevent="ChangeName">
                 <div class="form-group row">
-                  <label for="name" class="col-md-4 col-form-label text-md-right">New Name</label>
+                  <label for="name" class="col-md-4 col-form-label" style="text-align: left;">New Name:</label>
   
                   <div class="col-md-6">
                     <input
@@ -25,11 +25,37 @@
                 </div>
   
                 <div class="form-group row mb-0">
-                  <div class="col-md-8 offset-md-4">
+                  <div class="col-md-8 offset-md-4" style="margin-bottom: 30px;">
                     <v-btn type="submit" class="btn btn-primary">Change Name</v-btn>
                   </div>
                 </div>
               </form>
+
+              <div class="card-header">Change the password to login to your account.</div>
+              <form action="#" @submit.prevent="ChangePassword">
+                <div class="form-group row">
+                  <label for="password" class="col-md-4 col-form-label" style="text-align: left;">New Password:</label>
+  
+                  <div class="col-md-6">
+                    <input
+                      id="password"
+                      type="password"
+                      class="form-control"
+                      name="password"
+                      required
+                      v-model="password"
+                    />
+                  </div>
+                </div>
+  
+                <div class="form-group row mb-0">
+                  <div class="col-md-8 offset-md-4" style="margin-bottom: 10px;">
+                    <v-btn type="submit" class="btn btn-primary">Change Password</v-btn>
+                  </div>
+                </div>
+
+              </form>
+              
             </div>
             <div>
 
@@ -38,10 +64,10 @@
             <div class="card-header"><strong>Delete Your Account: The button below will delete your account. Once pressed all your information will be removed from this site.</strong></div>
             <div class="card-body">
               <div v-if="error" class="alert alert-danger">{{error}}</div>
-              <form action="#"  @click.prevent="RemoveAll()">
+              <form action="#" @submit.prevent="RemoveAll()">
                 <div class="form-group row mb-0">
                   <div class="col-md-8 offset-md-4">
-                    <v-btn type="submit" class="btn btn-primary">Delete Account</v-btn>
+                    <button type="submit" class="btn btn-primary" style="color: white; background-color: red; padding: 10px; border: none; border-radius: 4px; cursor: pointer;" onclick="RemoveAll()">Delete Account</button>
                   </div>
                 </div>
               </form>
@@ -64,6 +90,7 @@
     name: "SettingsComponent",
       setup() {
       const name = ref('')
+      const password = ref('')
       const error = ref(null)
 
       const store = useStore()
@@ -72,6 +99,15 @@
       const ChangeName = async () => {
         try {
           await store.dispatch('ChangeName', {name: name.value})
+          router.push('/Dashboard')
+        } catch (err) {
+          error.value = err.message
+        }
+      }
+
+      const ChangePassword = async () => {
+        try {
+          await store.dispatch('ChangePassword', {password: password.value})
           router.push('/Dashboard')
         } catch (err) {
           error.value = err.message
@@ -87,7 +123,7 @@
         }
       }
 
-      return {name, error, ChangeName, DeleteUser}
+      return {name, password, error, ChangeName, ChangePassword, DeleteUser}
     },
 
     data: () => ({
@@ -132,10 +168,24 @@
   </script>
 
 <style scoped>
-  input {
-    width: 50%;
-    padding: 8px;
-    margin-bottom: 16px;
-    border: 1px solid #ccc;
-  }
+.container {
+  max-width: 700px;
+  margin: auto;
+  padding: 20px;
+  border: 3px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+}
+
+label {
+  display: block;
+  margin-bottom: 8px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 16px;
+  border: 1px solid #ccc;
+}
 </style>
